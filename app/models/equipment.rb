3ab75930694c193399083b5a5106173aa2339da2
KevinImbott <kevin.imbott@epitech.eu>
2023-10-01 23:05:57 +0200

    feat(equipment): rarity working and setting the correct attribute depending of type

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,7 +1,7 @@
 class Equipment < ApplicationRecord
     self.table_name = "equipments"
 
-    TYPES = %w[Weapon Shield Helmet Gauntlet Boot Chest Gauntlet]
+    TYPES = %w[Weapon Shield Helmet Gauntlet Boot Chest]
 
     enum rarity: { common: 0, uncommon: 1, rare: 2, epic: 3, legendary: 4 }
 
@@ -13,6 +13,7 @@ class Equipment < ApplicationRecord
         random_rarity
         set_level
         random_type
+        set_attribute
         equip
         save
     end
@@ -36,6 +37,38 @@ class Equipment < ApplicationRecord
     def random_type
         return if type.present?
         self.type = TYPES.sample
+    end
+
+    def set_attribute
+        case type
+        when "Weapon"
+            self.attack = rarity_randomizer * level
+        when "Shield"
+            self.defense = rarity_randomizer * level
+        when "Helmet"
+            self.defense = rarity_randomizer * level
+        when "Gauntlet"
+            self.luck = rarity_randomizer * level
+        when "Boot"
+            self.speed = rarity_randomizer * level
+        else
+            self.defense = rarity_randomizer * level
+        end
+    end
+
+    def rarity_randomizer
+        case rarity
+        when "common"
+            rand(1..3)
+        when "uncommon"
+            rand(2..4)
+        when "rare"
+            rand(3..6)
+        when "epic"
+            rand(5..7)
+        when "legendary"
+            rand(7..10)
+        end
     end
 
     def unequip
